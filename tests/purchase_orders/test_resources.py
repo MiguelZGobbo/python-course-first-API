687f3bf8c33537fadafab07ab07074ff526dfc0f
Miguel Zager Gobbo <miguelzgobbo@gmail.com>
2025-03-19 13:53:15 -0300

    test post purchase_orders

--- tests/purchase_orders/test_resources.py
+++ tests/purchase_orders/test_resources.py
@@ -1,5 +1,17 @@
+import json
+
 def test_get_purchase_orders(test_client):
     response = test_client.get('/purchase_orders')
 
     assert response.status_code == 200
     assert response.json[0]['id'] == 1
+
+def test_post_purchase_orders(test_client):
+    obj = {'id': 2}
+    response = test_client.post(
+        data = json.dumps(obj),
+        contente_type = 'application/json'
+    )
+
+    assert response.status_code == 200
+    assert response.json['id'] == obj['id']
