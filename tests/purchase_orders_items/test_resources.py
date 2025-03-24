65834aa442975c910ea46ff6461727b6cf43edce
Miguel Zager Gobbo <miguelzgobbo@gmail.com>
2025-03-24 13:37:57 -0300

    add test_post_purchase_order_item

--- tests/purchase_orders_items/test_resources.py
+++ tests/purchase_orders_items/test_resources.py
@@ -1,3 +1,5 @@
+import json
+
 def test_get_items_purchase_order_id(test_client):
     response = test_client.get('/purchase_orders/1/items')
 
@@ -11,4 +13,17 @@ def test_get_items_purchase_order_id_not_found(test_client):
 
     assert response.status_code == 200
     assert response.json['message'] == 'Pedido de id:{} não encontrado!'.format(id)
-    
+
+def test_post_purchase_order_item(test_client):
+    obj = {'id': 2, 'description': '', 'price': int }
+
+    response = test_client.post(
+        '/purchase_order_items/1/items',
+        data = json.dumps(obj),
+        constext_type = 'application//json'
+    )
+     
+    assert response.status_code == 200
+    assert response.json['id'] == 1
+    assert len(response.json['items']) == 2
+    assert response.json['items'][1]['id'] == obj('id')
