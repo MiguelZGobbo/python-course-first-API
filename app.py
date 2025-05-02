9a74f1c49015be7837e0cffeb769ac080393b142
Miguel Zager Gobbo <miguelzgobbo@gmail.com>
2025-05-02 14:01:03 -0300

    refatorando os testes para purchase_order

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from purchase_orders.resources import PurchaseOrders, PurchaseOrdersById
@@ -5,17 +6,12 @@ from purchase_orders_items.resources import PurchaseOrdersItems
 from db import db
 from flask_migrate import Migrate
 
-def create_app(env):
+def create_app():
 
     app = Flask(__name__)
     api = Api(app)
 
-    database = 'python_course'
-
-    if env == 'testing':
-        database = 'python_course_test'
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/{}'.format(database)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URI']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
--- tests/purchase_orders/test_resources.py
+++ tests/purchase_orders/test_resources.py
@@ -6,9 +6,10 @@ def test_get_purchase_orders(test_client, seed_db):
     assert response.status_code == 200
     assert response.json[0]['id'] == seed_db.id
     assert response.json[0]['description'] == seed_db.description
+    assert response.json[0]['quantity']  == seed_db.quantity
 
 def test_post_purchase_orders(test_client):
-    obj = {'id': 2, 'description': ''}
+    obj = {'description': '', 'quantity': 150}
     response = test_client.post(
         '/purchase_orders',
         data = json.dumps(obj),
@@ -17,6 +18,8 @@ def test_post_purchase_orders(test_client):
 
     assert response.status_code == 200
     assert response.json['id'] is not None
+    assert response.json['description'] == obj['description']
+    assert response.json['quantity'] == obj['quantity']
 
 def test_post_empty_description(test_client):
     response = test_client.post(
@@ -33,6 +36,8 @@ def test_get_purchase_order_by_id(test_client, seed_db):
 
     assert response.status_code == 200
     assert response.json['id'] == seed_db.id
+    assert response.json['description'] == seed_db.description
+    assert response.json['quantity'] == seed_db.quantity
 
 def test_get_purchase_order_not_found(test_client):
     id = 999
